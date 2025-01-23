e09f46ae39fa0e1eea5f52b0424542c02ceab45f
Dhruv <erdhruv.sharma@gmail.com>
2025-01-24 01:27:15 +0530

    Search element in sorted array using B tree

---
+++ challenges/SearchElementInSortedOrRotatedArray.java
@@ -0,0 +1,43 @@
+package challenges;
+
+public class SearchElementInSortedOrRotatedArray {
+    // Java program to search an element in sorted and rotated
+        static int binarySearch(int[] arr, int lo, int hi, int x) {
+            while (lo <= hi) {
+                int mid = lo + (hi - lo) / 2;
+                if (arr[mid] == x) return mid;
+                if (arr[mid] < x) lo = mid + 1;
+                else hi = mid - 1;
+            }
+            return -1;
+        }
+        static int search(int[] arr, int key) {
+            int n = arr.length, pivot, lo = 0, hi = n-1;
+
+            while (lo < hi) {
+                if (arr[lo] <= arr[hi]) pivot = lo;
+                int mid = (lo + hi) / 2;
+                if (arr[mid] > arr[hi])
+                    lo = mid + 1;
+                else
+                    hi = mid;
+            }
+            pivot = lo;
+
+            if (arr[pivot] == key)
+                return pivot;
+
+            if (pivot == 0)
+                return binarySearch(arr, 0, n - 1, key);
+
+            if (arr[0] <= key)
+                return binarySearch(arr, 0, pivot - 1, key);
+            return binarySearch(arr, pivot + 1, n - 1, key);
+        }
+
+        public static void main(String[] args) {
+            int[] arr = {5, 6, 7, 8, 9, 10, 1, 2, 3};
+            int key = 3;
+            System.out.println(search(arr, key));
+        }
+}
--- challenges/Thread1a2b3c.java
+++ challenges/Thread1a2b3c.java
@@ -4,40 +4,41 @@ public class Thread1a2b3c {
     static final Object lock = new Object();
     static boolean numberTurn = true;
     public static void main(String[] args) {
-        var print_numbers = new Thread(()->{
-            for (int i = 1; i <= 26; i++) {
-                synchronized (lock) {
-                    while (!numberTurn) {
-                        try{
-                            lock.wait();
-                        } catch (InterruptedException _) {
-                            Thread.currentThread().interrupt();
-                        }
-                    }
-                    System.out.print(i);
-                    numberTurn=false;
-                    lock.notifyAll();
-                }
-            }
-        });
-        var print_chars = new Thread(()-> {
-            for (char c='A';c<='Z';c++) {
-                synchronized (lock) {
-                    while (numberTurn) {
-                        try {
-                            lock.wait();
-                        } catch (InterruptedException _) {
-                            Thread.currentThread().interrupt();
-                        }
-                    }
-                    System.out.print(c);
-                    numberTurn = true;
-                    lock.notifyAll();
-                }
-            }
-        });
+        new Thread(()->print_num()).start();
+        new Thread(()->print_char()).start();
+    }
 
-        print_numbers.start();
-        print_chars.start();
+    private static void print_char() {
+        for (char c='A';c<='Z';c++) {
+            synchronized (lock) {
+                while (numberTurn) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException _) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+                System.out.print(c);
+                numberTurn = true;
+                lock.notifyAll();
+            }
+        }
+    }
+
+    private static void print_num() {
+        for (int i = 1; i <= 30; i++) {
+            synchronized (lock) {
+                while (!numberTurn) {
+                    try{
+                        lock.wait();
+                    } catch (InterruptedException _) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+                System.out.print(i);
+                numberTurn=false;
+                lock.notifyAll();
+            }
+        }
     }
 }
