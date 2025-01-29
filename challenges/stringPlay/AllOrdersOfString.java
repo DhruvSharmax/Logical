dd7d5363b9fbfd7a1fe61c8bc997959339109e3f
Dhruv <erdhruv.sharma@gmail.com>
2025-01-29 08:15:26 +0530

    update

--- challenges/stringPlay/AllOrdersOfString.java
+++ challenges/stringPlay/AllOrdersOfString.java
@@ -3,7 +3,7 @@ package challenges.stringPlay;
 /*
  * https://www.geeksforgeeks.org/write-a-c-program-to-print-all-permutations-of-a-given-string/
  */
-public class AllPermutationsString {
+public class AllOrdersOfString {
 	public static void main(String[] args) {
 		printPermutations("abc");
 	}
