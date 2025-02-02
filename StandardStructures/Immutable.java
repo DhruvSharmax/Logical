daf82f67f1667327dd65f1fc63541f0cbcab0238
Dhruv <41086944+DhruvSharmax@users.noreply.github.com>
2025-02-02 19:35:26 +0530

    Update Immutable.java

--- StandardStructures/Immutable.java
+++ StandardStructures/Immutable.java
@@ -3,10 +3,15 @@ package StandardStructures;
 public final class Immutable {
 	private final int data1;
 	private final String str;
+ private final Student student;
 	
-		public Immutable(int data1,String str) {
+		public Immutable(int data1,String str, Student student) {
 			this.data1 = data1;
 			this.str = str;
+   Student temp = new Student();
+   temp.setA(1);
+   temp.setB(2);
+   this.student = temp;  // performed deep copy for mutable object
 		}
 		
 		public int getData1() {
@@ -16,5 +21,30 @@ public final class Immutable {
 		public String getStr() {
 			return str;
 		}
+
+  public Student getStudent() {
+ return student;
+}
 	}
+
+class Student {
+int a;
+int b;
+
+public int getA(){
+  return a;
+}
+public int getB(){
+  return b;
+}
+
+public void setA(int a) {
+  this.a=a;
+}
+
+public void setB(int b) {
+  this.b=b;
+}
+
+}
 	
